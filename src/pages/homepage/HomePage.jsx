08f0838c8774304d47597d27abf6cda4b451f494
proger01 <dr.pustowit2013@gmai.com>
2020-03-29 22:10:03 +0300

    styling menu-item component

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.scss';
+import {Directory} from '../../components/directory/Directory';
+
+export const HomePage = () => {
+	return (
+		<div className='homepage'>
+			<Directory />
+		</div>
+	)
+}
